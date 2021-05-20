6dfc6c8ae77fac843ebdde2fc44920b5e52e171f
Lorena Hernandez <hlore276@gamail.com>
2021-05-20 18:13:43 -0500

    fin seccion 13

--- routes/auth.js
+++ routes/auth.js
@@ -4,8 +4,9 @@
 
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { login, googleSingIn } = require('../controllers/auth');
+const { login, googleSingIn, renewToken } = require('../controllers/auth');
 const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
 
 const router = Router();
 
@@ -24,6 +25,10 @@ router.post( '/google',
       validarCampos
     ],
     googleSingIn
+);
+router.get( '/renew',
+    validarJWT,
+    renewToken
 )
 
 module.exports = router;
--- routes/hospitales.js
+++ routes/hospitales.js
@@ -31,11 +31,16 @@ crearHospital
 );
 
 router.put( '/:id', 
-[ ],
+[ 
+    validarJWT,
+    check('nombre', 'El nombre del hospital es necesario').not().isEmpty(),
+    validarCampos
+],
 actualizarHospital
 );
 router.delete( '/:id', 
-borrarHospital
+    validarJWT,
+    borrarHospital
 );
 
 module.exports = router;
